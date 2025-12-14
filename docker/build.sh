e6e6aada254bf6db623778927e6c67e9664f6b4e
Engin Tola <engintola@gmail.com>
2025-12-14 12:23:08 +0300

    added nocuda dockerfile

--- docker/build.sh
+++ docker/build.sh
@@ -3,8 +3,25 @@ set -e
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )"/.. && pwd )"
 
-echo $DIR
+# Parse arguments
+DISABLE_CUDA=false
+for arg in "$@"; do
+    case $arg in
+        --disable-cuda)
+            DISABLE_CUDA=true
+            shift
+            ;;
+    esac
+done
 
-docker build -t="glomap:local" --build-arg CUDA_ARCHITECTURES=75 ${DIR} -f ${DIR}/docker/Dockerfile
-# In some cases, you may have to explicitly specify the compute architecture:
-#   docker build -t="glomap:latest" --build-arg CUDA_ARCHITECTURES=native .
+echo "Building from: $DIR"
+
+if [ "$DISABLE_CUDA" = true ]; then
+    echo "Building without CUDA support..."
+    docker build -t="glomap:local-nocuda" ${DIR} -f ${DIR}/docker/Dockerfile.nocuda
+else
+    echo "Building with CUDA support..."
+    docker build -t="glomap:local" --build-arg CUDA_ARCHITECTURES=75 ${DIR} -f ${DIR}/docker/Dockerfile
+    # In some cases, you may have to explicitly specify the compute architecture:
+    #   docker build -t="glomap:latest" --build-arg CUDA_ARCHITECTURES=native .
+fi
